add584a35aa04953927fc7576b6c8cf8b2a6a3f1
mo-abujabal <mohamed01279055834@gmail.com>
2024-10-04 16:11:48 +0200

    update notifications

--- lib/features/Notification/Presentation/widgets/no_notifications.dart
+++ lib/features/Notification/Presentation/widgets/no_notifications.dart
@@ -1,9 +1,6 @@
-import 'package:car_help/core/utils/app_assets.dart';
-import 'package:car_help/core/utils/app_colors.dart';
 import 'package:car_help/core/utils/app_styles.dart';
 import 'package:car_help/generated/l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:iconsax/iconsax.dart';
 
 class NoNotifications extends StatelessWidget {
